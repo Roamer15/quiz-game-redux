1cfaa7469db864e094c2e6444dbf3d0350df1acf
Roamer15 <ianseeksbeta@gmail.com>
2025-02-05 14:43:31 +0100

    Creation of pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router'
+import LandingPage from './pages/LandingPage'
+import QuizPage from './pages/QuizPage'
+import ResultPage from './pages/ResultPage'
+import { Provider } from "react-redux"
 
+function App() {
+  return (
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/quizpage/:number' element={<QuizPage />} />
+          <Route path='/results' element={<ResultPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  )
 }
 
 export default App
